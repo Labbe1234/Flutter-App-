3ea61a158574938a5d0358f23c9f99d6e72845c9
IgnacioBello1 <ibello@miuandes.cl>
2024-06-03 20:13:40 -0400

    camera model

--- lib/Widgets/camera_page.dart
+++ lib/Widgets/camera_page.dart
@@ -1,9 +1,11 @@
-// import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite_v2/tflite_v2.dart';
+import 'package:image/image.dart' as img;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,16 +31,17 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
   File? file;
   var _recognitions;
   var v = "";
-  // var dataList = [];
+  img.Image? preprocessedImg;
+
   @override
   void initState() {
     super.initState();
-    loadmodel().then((value) {
+    loadModel().then((value) {
       setState(() {});
     });
   }
 
-  loadmodel() async {
+  loadModel() async {
     await Tflite.loadModel(
       model: "assets/model_original2.tflite",
       labels: "assets/labels.txt",
@@ -48,51 +51,93 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
   Future<void> _pickImageCamera() async {
     try {
       final XFile? image = await _picker.pickImage(source: ImageSource.camera);
-      setState(() {
-        _image = image;
-        file = File(image!.path);
+      if (image != null) {
+        File originalFile = File(image.path);
 
-      });
-      detectimage(file!);
+        // Load the image using the image package
+        img.Image originalImage = img.decodeImage(originalFile.readAsBytesSync())!;
+
+        // Resize the image to 96x96
+        img.Image resizedImage = img.copyResize(originalImage, width: 96, height: 96);
+
+        // Save the resized image to a temporary file
+        String tempPath = (await getTemporaryDirectory()).path;
+        File tempFile = File('$tempPath/temp_image.png')..writeAsBytesSync(img.encodePng(resizedImage));
+
+        setState(() {
+          _image = image;
+          file = tempFile;
+          preprocessedImg = resizedImage;
+        });
+
+        detectImage(file!);
+      }
     } catch (e) {
       print('Error picking image: $e');
     }
   }
 
- Future<void> _pickImageGallery() async {
+  Future<void> _pickImageGallery() async {
     try {
       final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-      setState(() {
-        _image = image;
-        file = File(image!.path);
+      if (image != null) {
+        File originalFile = File(image.path);
 
-      });
-      detectimage(file!);
+        // Load the image using the image package
+        img.Image originalImage = img.decodeImage(originalFile.readAsBytesSync())!;
+
+        // Resize the image to 96x96
+        img.Image resizedImage = img.copyResize(originalImage, width: 96, height: 96);
+
+        // Save the resized image to a temporary file
+        String tempPath = (await getTemporaryDirectory()).path;
+        File tempFile = File('$tempPath/temp_image.png')..writeAsBytesSync(img.encodePng(resizedImage));
+
+        setState(() {
+          _image = image;
+          file = tempFile;
+          preprocessedImg = resizedImage;
+        });
+
+        detectImage(file!);
+      }
     } catch (e) {
       print('Error picking image: $e');
     }
   }
 
-  Future detectimage(File image) async {
-    int startTime = new DateTime.now().millisecondsSinceEpoch;
+  Future<void> detectImage(File image) async {
+    int startTime = DateTime.now().millisecondsSinceEpoch;
+
     var recognitions = await Tflite.runModelOnImage(
       path: image.path,
-      // numResults: 1,
-      // threshold: 0.65,
-      // imageMean: 127.5,
-      // imageStd: 127.5,
+      numResults: 2, // Return 1 or 2 results
+      threshold: 0.65, // Confidence threshold
     );
+
     setState(() {
       _recognitions = recognitions;
       v = recognitions?.map((recognition) => recognition['label']).join(', ') ?? '';
-      // dataList = List<Map<String, dynamic>>.from(jsonDecode(v));
     });
+
     print("//////////////////////////////////////////////////");
     print(_recognitions);
-    // print(dataList);
     print("//////////////////////////////////////////////////");
-    int endTime = new DateTime.now().millisecondsSinceEpoch;
+
+    int endTime = DateTime.now().millisecondsSinceEpoch;
     print("Inference took ${endTime - startTime}ms");
+  }
+
+  Widget _displayPreprocessedImage() {
+    if (preprocessedImg == null) {
+      return Text("No preprocessed image available.");
+    } else {
+      return Image.memory(
+        Uint8List.fromList(img.encodePng(preprocessedImg!)),
+        width: 96,
+        height: 96,
+      );
+    }
   }
 
   @override
@@ -111,21 +156,23 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
               )
             else
               Text('No image selected'),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: _pickImageCamera,
-            child: Text('Use Camera'),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: _pickImageGallery,
-            child: Text('Pick from Gallery'),
-          ),
-          SizedBox(height: 20),
-          Text(v),
-        ],
+            SizedBox(height: 20),
+            if (preprocessedImg != null) _displayPreprocessedImage(),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pickImageCamera,
+              child: Text('Use Camera'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pickImageGallery,
+              child: Text('Pick from Gallery'),
+            ),
+            SizedBox(height: 20),
+            Text(v),
+          ],
         ),
-     ),
+      ),
     );
   }
 }
